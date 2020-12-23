3759a5d770e4f4dce8306a5688bb559eea48b029
innagine <57307985+innagine@users.noreply.github.com>
2020-12-24 00:54:27 +0800

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,31 @@ const handleBlogRouter = require('./src/router/blog')
 const handleUserRouter = require('./src/router/user')
 
 
+//用于处理post data
+const getPostData = (res)=>{
+    const promise = new Promise((resolve,reject) =>{
+        if(req.method !== 'POST'){
+            resolve({})
+            return
+        }
+        if(req.headers['content-type'] !== 'application/json'){
+            resolve({})
+            return
+        }
+        let postData=''
+        req.on('data',chunk =>{
+            postData=chunk.toString()
+        })
+        res.on('end',()=>{
+            if(!postData){
+                resolve({})
+            }
+            resolve(JSON.parse(postData))
+        }) 
+    })
+    return promise
+}
+
 
 const serverHandle = (req,res) => {
     //设置返回格式json
@@ -15,28 +40,35 @@ const serverHandle = (req,res) => {
     // 解析query
     req.query = querystring.parse(url.split('?')[0])
 
-    //处理blog路由
-    const blogData = handleBlogRouter(req,res)
-    if(blogData){
-        res.end(
-            JSON.stringify(blogData)
-        )
-        return
-    } 
+    //处理post data
+    getPostData(res).then(postData =>{
+        req.body = postData
 
-    // 处理user路由
-    const userData = handleUserRouter(req,res)
-    if(userData){
-        res.end(
-            JSON.stringify(userData)
-        )
-        return
-    }
+        //处理blog路由
+        const blogData = handleBlogRouter(req,res)
+        if(blogData){
+            res.end(
+                JSON.stringify(blogData)
+            )
+            return
+        } 
+    
+        // 处理user路由
+        const userData = handleUserRouter(req,res)
+        if(userData){
+            res.end(
+                JSON.stringify(userData)
+            )
+            return
+        }
+    
+        //未命中路由，返回404
+        res.writeHead(404,{"Content-type":"text/plain"})
+        res.write("404 Not Found\n")
+        res.end()
+    })
 
-    //未命中路由，返回404
-    res.writeHead(404,{"Content-type":"text/plain"})
-    res.write("404 Not Found\n")
-    res.end()
+    
 
 }
 module.exports = serverHandle
